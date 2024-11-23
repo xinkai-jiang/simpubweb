4ba5bc1b1701d2d8b52b8b921d02d78e0b0b4eca
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-11-23 19:14:11 +0100

    dash board receive and update

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
+from flask import Flask
 from app import create_app
+# from .app.routes import main
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
